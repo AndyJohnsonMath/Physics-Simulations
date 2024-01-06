0c7f5acc12644652a41cbd97c9aab6b77e2021bd
Andy Johnson <130934408+AndyJohnsonMath@users.noreply.github.com>
2024-01-06 14:51:51 -0500

    Minor changes

--- memoryExperiment.py
+++ memoryExperiment.py
@@ -8,6 +8,10 @@
 # 4) Wait for 3) to finish
 # 5) run: python3 -m mprof plot
 
+# On my laptop I have to run: python -m mprof run memoryExperiment.py
+# On my desktop I have to run: python3 -m mprof run memoryExperiment.py
+# No clue why i need the 3 in the desktop but not on the laptop, but thats what i need to do
+
 # Final result is a plot of the memory consumption (In MiB) over time taken at 0.1s intervals
 
 # Necessary libraries, just a hodgepodge of ones ive used
@@ -18,9 +22,9 @@ import numpy as np
 # definition body of memFunc() is the function you actually want to run
 def memFunc():
     i = 0
-    while i <= 200:
-        sim.gravitySimulation(numParticles=(i+2), numFrames=10)
-        i += 10
+    while i <= 70:
+        sim.gravitySimulation(numParticles=(i+48), numFrames=300)
+        i += 5
 
 # Need this __name__ conditional to run the memFunc() function
 if __name__ == '__main__':
